8207f518967c7f6e804a1e04b4338f6ef9a6493c
Sanni Abdulmusawwir <sanniabdulmusawwir@gmail.com>
2017-01-23 22:10:33 +0000

    add doc blocks to htmlparser

--- src/HtmlParser.php
+++ src/HtmlParser.php
@@ -9,7 +9,18 @@ use Symfony\Component\DomCrawler\Crawler as DOMCrawler;
 
 class HtmlParser
 {
+    /**
+     * object for creating DOM crawler objects.
+     *
+     * @var SiteCrawler\Factories\DOMFactory
+     */
     protected $domFactory;
+
+    /**
+     * object for creating URL objects
+     *
+     * @var SiteCrawler\Factories\UrlFactory
+     */
     protected $urlFactory;
 
     public function __construct(DOMFactory $domFactory, UrlFactory $urlFactory)
@@ -18,11 +29,26 @@ class HtmlParser
         $this->urlFactory = $urlFactory;
     }
 
+    /**
+     * makes a DOM crawler object representing the HTML page at the supplied URL
+     *
+     * @param  Url    $pageUrl
+     * @param  string $html
+     *
+     * @return Symfony\Component\DomCrawler\Crawler
+     */
     public function makeDocument(Url $pageUrl, $html)
     {
         return $this->domFactory->makeDOMCrawler($pageUrl, $html);
     }
 
+    /**
+     * gets the URLs of all the links present in the supplied DOM object
+     *
+     * @param  Symfony\Component\DomCrawler\Crawler $domCrawler
+     *
+     * @return array
+     */
     public function getLinks(DOMCrawler $domCrawler)
     {
         $links = $domCrawler->filter("a")->links();
@@ -32,6 +58,13 @@ class HtmlParser
         return $links;
     }
 
+    /**
+     * gets the URLs of all the images present in the supplied DOM object
+     *
+     * @param  Symfony\Component\DomCrawler\Crawler $domCrawler
+     *
+     * @return array
+     */
     public function getImages(DOMCrawler $domCrawler)
     {
         $images = $domCrawler->filter("img")->images();
@@ -41,6 +74,13 @@ class HtmlParser
         return $images;
     }
 
+    /**
+     * gets the URLs of all the icons present in the supplied DOM object
+     *
+     * @param  Symfony\Component\DomCrawler\Crawler $domCrawler
+     *
+     * @return array
+     */
     public function getIcons(DOMCrawler $domCrawler)
     {
         $icons = $domCrawler->filter("link[rel=icon]")->links();
@@ -54,6 +94,13 @@ class HtmlParser
         return $icons;
     }
 
+    /**
+     * gets the URLs of all the stylesheets present in the supplied DOM object
+     *
+     * @param  Symfony\Component\DomCrawler\Crawler $domCrawler
+     *
+     * @return array
+     */
     public function getStyleSheets(DOMCrawler $domCrawler)
     {
         $styleSheets = $domCrawler->filter("link[rel=stylesheet]")->links();
@@ -63,6 +110,13 @@ class HtmlParser
         return $styleSheets;
     }
 
+    /**
+     * gets the URLs of all the scripts present in the supplied DOM object
+     *
+     * @param  Symfony\Component\DomCrawler\Crawler $domCrawler
+     *
+     * @return array
+     */
     public function getScripts(DOMCrawler $domCrawler)
     {
         $scripts = [];
@@ -79,6 +133,13 @@ class HtmlParser
         return $scripts;
     }
 
+    /**
+     * gets the URLs of all the assets(images, icons, stylesheets and scripts) present in the supplied DOM object
+     *
+     * @param  Symfony\Component\DomCrawler\Crawler $domCrawler
+     *
+     * @return array
+     */
     public function getAssets(DOMCrawler $domCrawler)
     {
         return array_merge(
